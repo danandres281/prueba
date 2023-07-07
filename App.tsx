fc00982c6a63db141a7e08dbdd48b61e3d87b4f4
danandres281 <64227662+danandres281@users.noreply.github.com>
2023-07-06 20:22:02 -0500

    Subiendo prueba tecnica
    Se desarrollo la apliacion de tareas con cuenta regresiva y notificaciones en React Native

---
+++ App.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { AplicacionTareasScreen } from './android/app/sources/screens/AplicacionTareasScreen';
+
+
+export const App = () => {
+    return (
+     <AplicacionTareasScreen/>
+    )
+}
+
